0fe033d22e226b68a7ee89ef12ca8505c098151c
Diogo Florêncio <diogo.lima@ccc.ufcg.edu.br>
2017-05-04 18:21:46 -0300

    inputDate ok

--- service/main.js
+++ service/main.js
@@ -4,19 +4,46 @@ var infowindow = [];
 var markers = [];
 var i = 1;
 
-function initMap() {
-       
+var dateInput = document.getElementById('dateInput');
+
+function initMap() {     
   map = new google.maps.Map(document.getElementById('map'), {
         zoom: 16,
         center: home
   });
-
-  var centerControlDiv = document.getElementById('dateInput');
-
-        centerControlDiv.index = 1;
-        map.controls[google.maps.ControlPosition.LEFT_TOP].push(centerControlDiv);
-
+  setInputDate();
   loadingData();
+}
+
+function setInputDate(){
+  posicaoInputDate(dateInput, map);
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(dateInput);
+}
+
+function posicaoInputDate(dateInput, map) {
+
+        // Set CSS for the control border.
+        var controlUI = document.getElementById('dateInput');
+        controlUI.style.backgroundColor = '#fff';
+        controlUI.style.border = '2px solid #fff';
+        controlUI.style.borderRadius = '3px';
+        controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+        controlUI.style.cursor = 'pointer';
+        controlUI.style.marginBottom = '22px';
+        controlUI.style.textAlign = 'center';
+        controlUI.title = 'Click to recenter the map';
+        dateInput.appendChild(controlUI);
+
+        // Set CSS for the control interior.
+        var controlText = document.getElementById('dateInput');
+        controlText.style.color = 'rgb(25,25,25)';
+        controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+        controlText.style.fontSize = '16px';
+        controlText.style.lineHeight = '38px';
+        controlText.style.paddingLeft = '5px';
+        controlText.style.paddingRight = '5px';
+        controlText.innerHTML = 'Center Map';
+        controlUI.appendChild(controlText);
 }
 
 function loadingData(){
