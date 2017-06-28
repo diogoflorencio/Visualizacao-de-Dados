2c15c539552f9870ddf5455738a91c803266eeb2
Renato Ely <renato.silva@ccc.ufcg.edu.br>
2017-06-27 22:40:19 -0300

    Timeline com cores

--- POETA/scripts/timeline.js
+++ POETA/scripts/timeline.js
@@ -1,3 +1,6 @@
+const coresAtividades=['darkblue','lightgreen','lightblue','darkgreen','darkorange','lightpurple',
+						'darkred','lightorange','darkpurple','lightred','darkgold','lightgold'];
+const corSobreposicao="sobreposicao";
 
 function exibirTimeline(d){
 const element = document.getElementById('timelineGraf');
@@ -10,13 +13,26 @@ var atividades = getActivitiesName(d);
 var prazos = getActivitiesPrazos(d,filhos);
 
 var data = [];
+
+var previsoes=[];
+for(var i = 0; i < atividades.length; i++){
+	previsoes.push({label: atividades[i],
+					type: TimelineChart.TYPE.INTERVAL,
+					from: converteData(prazos[i].inicio),
+					to: makeDataWithTime(prazos[i].fim),
+					customClass: coresAtividades[(i)%coresAtividades.length]});
+}
+data.push({label: "",
+			data: previsoes});
+
 for(var i = 0; i < filhos.length; i++){
 	var entregas=[];
 	for(var j = 1; j < d.depth; j++){
 		entregas.push({label: atividades[j-1],
 						type: TimelineChart.TYPE.INTERVAL,
 						from: converteData(filhos[i]["Data Inicio "+j]),
-						to: makeDataWithTime(filhos[i]["Data Fim "+j])});
+						to: makeDataWithTime(filhos[i]["Data Fim "+j]),
+						customClass: coresAtividades[(j-1)%coresAtividades.length]});
 	}
 	data.push({label: filhos[i]["Level18"],
 				data: entregas});
