1614ffd6bfb6cd5f31d536429bc999aba727ce16
Diogo Florêncio <diogo.lima@ccc.ufcg.edu.br>
2018-10-04 10:48:46 -0300

    add dados processados

--- pos/lab1/vis-lab1.R
+++ pos/lab1/vis-lab1.R
@@ -1,8 +1,18 @@
 library(tidyverse)
 
-authors <- read.csv("~/Downloads/authors.csv")
-papers <- read.csv("~/Downloads/papers.csv")
-conferences <- read.csv("~/Downloads/conferences.csv")
-ethnicity <- read.csv("~/Downloads/ethnicity.csv")
+authors <- read.csv("authors.csv")
+papers <- read.csv("papers.csv")
+conferences <- read.csv("conferences.csv")
+ethnicity <- read.csv("ethnicity.csv")
 
+pub_year <- papers %>% select(paper_key, conf_key) %>% group_by(paper_key) %>% merge(conferences) %>% 
+          select(year) %>% group_by(year) %>% count()
 
+conf_year <- papers %>% select(paper_key, conf_key) %>% group_by(paper_key) %>% merge(conferences) %>% 
+  select(global_key) %>% group_by(global_key) %>% count() %>% ungroup() %>% top_n(10)
+
+author_pub <- authors %>% select(name_author) %>% group_by(name_author) %>% count() %>% ungroup() %>% top_n(10)
+
+write.csv (pub_year, file = "pub_year.csv")
+write.csv (conf_year, file = "conf_year.csv")
+write.csv (author_pub, file = "author_pub.csv")
