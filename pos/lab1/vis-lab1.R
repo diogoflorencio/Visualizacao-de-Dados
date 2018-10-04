479d01b5a8545bb91631ae9362c7896c1ebcd52e
Diogo Florêncio <diogo.lima@ccc.ufcg.edu.br>
2018-10-03 23:23:06 -0300

    add rep pos

---
+++ pos/lab1/vis-lab1.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+
+authors <- read.csv("~/Downloads/authors.csv")
+papers <- read.csv("~/Downloads/papers.csv")
+conferences <- read.csv("~/Downloads/conferences.csv")
+ethnicity <- read.csv("~/Downloads/ethnicity.csv")
+
+
