cded3f910ed9f87347c1500f22b3d9c866d07cc4
Diogo Florêncio <diogo.lima@ccc.ufcg.edu.br>
2018-10-26 12:19:06 -0300

    redesing

---
+++ pos/redesigns/redesign-1.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(lubridate)
+bigMac <- read.csv("big-mac-source-data.csv")
+bigMac <-mutate(bigMac, price_dollar = local_price/dollar_ex) %>%
+         select(name, price_dollar, date) %>%
+         filter(name %in% c("Switzerland", "Norway", "Sweden", "United States",
+                            "Canada", "Brazil", "Denmark", "Australia", "New Zealand",
+                            "Britain", "Czech Republic", "Japan", "China", "Poland",
+                            "Turkey", "Russia", "South Africa"))
+
+bigMac <- bigMac%>%
+          group_by(name) %>%
+          mutate(var_perc = 100 * (price_dollar - lag(price_dollar))/lag(price_dollar)) %>%
+          select(name, var_perc)
