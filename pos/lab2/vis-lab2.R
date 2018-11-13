30a2f02981e08251a6515527a5700c03bcdea7ae
Diogo Florêncio <diogo.lima@ccc.ufcg.edu.br>
2018-11-13 07:37:41 -0300

    lab2 script

--- pos/lab2/vis-lab2.R
+++ pos/lab2/vis-lab2.R
@@ -4,8 +4,28 @@ enade_pb <- read.csv("enade_2017_pb.csv")
 
 enade_pb <- select(enade_pb, CO_CATEGAD, QE_I15, QE_I21) 
 
-vis1 <- enade_pb %>%
+dist_cotas <- enade_pb %>%
         group_by(CO_CATEGAD, QE_I15) %>%
         summarise(contagem = dplyr::n()) %>% 
-        mutate(percentual = contagem / sum(contagem))
-vis1
+        mutate(percentual = contagem / sum(contagem)) %>%
+        filter(QE_I15 ==  'A') %>%
+        mutate(sem_conta = percentual, com_cota = 1-percentual) %>%
+        select(CO_CATEGAD, com_cota, sem_conta)
+
+sem_cota <- enade_pb %>% filter(QE_I15 == 'A') %>%
+        group_by(CO_CATEGAD, QE_I21) %>%
+        summarise(contagem = dplyr::n()) %>% 
+        mutate(percentual = contagem / sum(contagem)) %>%
+        filter(QE_I21 == "A") %>% 
+        mutate(sem_cota_fam_grad = percentual, sem_cota_sem_fam_grad = 1- percentual) %>%
+        select(CO_CATEGAD, sem_cota_fam_grad, sem_cota_sem_fam_grad)
+
+com_cota <- enade_pb %>% filter(QE_I15 != 'A') %>%
+            group_by(CO_CATEGAD, QE_I21) %>%
+            summarise(contagem = dplyr::n()) %>% 
+            mutate(percentual = contagem / sum(contagem)) %>%
+            filter(QE_I21 == "A") %>% 
+            mutate(cota_fam_grad = percentual, cota_sem_fam_grad = 1- percentual) %>%
+            select(CO_CATEGAD, cota_fam_grad, cota_sem_fam_grad)
+
+dist_fam_grad <- merge(sem_cota, com_cota)
