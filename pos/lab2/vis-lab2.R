eec80a5b9fb3879ae03a58692b09d45ec86d81cf
Diogo Florêncio <diogo.lima@ccc.ufcg.edu.br>
2018-11-13 10:42:27 -0300

    dados vis 1

--- pos/lab2/vis-lab2.R
+++ pos/lab2/vis-lab2.R
@@ -2,30 +2,24 @@ library(tidyverse)
 
 enade_pb <- read.csv("enade_2017_pb.csv")
 
-enade_pb <- select(enade_pb, CO_CATEGAD, QE_I15, QE_I21) 
+enade_pb <- select(enade_pb, CO_CATEGAD, QE_I15, QE_I21) %>% filter(QE_I15 != 'NA')
+
+enade_pb$QE_I15 <- as.character(enade_pb$QE_I15)
+enade_pb$QE_I15[enade_pb$QE_I15 == "A"] <- "Sem cota"
+enade_pb$QE_I15[enade_pb$QE_I15 != "Sem cota"] <- "Com cota"
 
 dist_cotas <- enade_pb %>%
         group_by(CO_CATEGAD, QE_I15) %>%
         summarise(contagem = dplyr::n()) %>% 
-        mutate(percentual = contagem / sum(contagem)) %>%
-        filter(QE_I15 ==  'A') %>%
-        mutate(sem_conta = percentual, com_cota = 1-percentual) %>%
-        select(CO_CATEGAD, com_cota, sem_conta)
+        mutate(percentual_aluno = contagem / sum(contagem)) %>%
+        select(CO_CATEGAD, QE_I15, percentual_aluno)
 
-sem_cota <- enade_pb %>% filter(QE_I15 == 'A') %>%
-        group_by(CO_CATEGAD, QE_I21) %>%
+dist_grad <- enade_pb %>% 
+        group_by(CO_CATEGAD, QE_I15, QE_I21) %>%
         summarise(contagem = dplyr::n()) %>% 
-        mutate(percentual = contagem / sum(contagem)) %>%
-        filter(QE_I21 == "A") %>% 
-        mutate(sem_cota_fam_grad = percentual, sem_cota_sem_fam_grad = 1- percentual) %>%
-        select(CO_CATEGAD, sem_cota_fam_grad, sem_cota_sem_fam_grad)
+        mutate(percentual_grad = contagem / sum(contagem)) %>%
+        filter(QE_I21 == "A") %>%
+        select(CO_CATEGAD, QE_I15, percentual_grad)
 
-com_cota <- enade_pb %>% filter(QE_I15 != 'A') %>%
-            group_by(CO_CATEGAD, QE_I21) %>%
-            summarise(contagem = dplyr::n()) %>% 
-            mutate(percentual = contagem / sum(contagem)) %>%
-            filter(QE_I21 == "A") %>% 
-            mutate(cota_fam_grad = percentual, cota_sem_fam_grad = 1- percentual) %>%
-            select(CO_CATEGAD, cota_fam_grad, cota_sem_fam_grad)
-
-dist_fam_grad <- merge(sem_cota, com_cota)
+dados <- merge(dist_cotas, dist_grad)    
+write.csv (dados, file = "dados-lab2-vis-1.csv")
